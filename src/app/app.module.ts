4e117fe8c9c9dac1cba22ce5898ff5363d19ad3d
Diane Xie <serenayang@MrAdmins-iMac.local>
2018-05-21 10:59:48 -0400

    add reorder function and toggole button in the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TodosPage } from '../pages/todos/todos';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    TodosPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    TodosPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public todos = [];
+  public reorderIsEnabled = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private alertController: AlertController) {
 
   }
 
+  openTodoAlert(){
+    let addTodoAlert = this.alertController.create({
+      title: "Add A Todo",
+      message: "Enter Your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addtodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData) => {
+            let todoText;
+            todoText = inputData.addtodoInput;
+            this.todos.push(todoText);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
+
+  }
+
+  toggleReorder(){
+    this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
 }
+
